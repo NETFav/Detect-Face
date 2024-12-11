a7be8e70f770886db70c228bced932e134252c76
CodeDev <130955280+NETFav@users.noreply.github.com>
2024-12-11 12:50:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify
 import cv2
 import numpy as np  # Don't forget to import numpy
 from flask_cors import CORS
-CORS(app)
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the pre-trained face detection model
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
